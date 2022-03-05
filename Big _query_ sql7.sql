15686b4bfb5f7cbcc007de0a4afec08d58e3c3fd
devisettyrohita <100766371+devisettyrohita@users.noreply.github.com>
2022-03-05 22:54:31 +0530

    Update and rename Big _query_ sql7 to Big _query_ sql7.sql

--- Big _query_ sql7.sql
+++ Big _query_ sql7.sql
@@ -1,4 +1,4 @@
-what are the top 10 liquors in Iowa in the dataset?
+#what are the top 10 liquors in Iowa in the dataset?
 
 SELECT item_description, sum(volume_sold_gallons) as Total_sales FROM `bigquery-public-data.iowa_liquor_sales.sales` 
 group by item_description
