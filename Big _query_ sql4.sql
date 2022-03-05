97bbb4b626695e357799c68f37bc7496ce5eb9bb
devisettyrohita <100766371+devisettyrohita@users.noreply.github.com>
2022-03-05 22:53:24 +0530

    Update and rename Big _query_ sql4 to Big _query_ sql4.sql

--- Big _query_ sql4.sql
+++ Big _query_ sql4.sql
@@ -1,4 +1,4 @@
-4. what are the total no. of deaths and new current confirmed cases dated between 2020-10-28 and  2021-01-06?
+#what are the total no. of deaths and new current confirmed cases dated between 2020-10-28 and  2021-01-06?
 
 SELECT deaths FROM `bigquery-public-data.covid19_italy.national_trends`
 where date between "2020-04-11" and "2021-11-19"
