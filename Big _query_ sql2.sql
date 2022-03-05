9e99171fec5462fa774d99b771750054897f23e3
devisettyrohita <100766371+devisettyrohita@users.noreply.github.com>
2022-03-05 22:51:31 +0530

    Update and rename Big _query_ sql2 to Big _query_ sql2.sql

--- Big _query_ sql2.sql
+++ Big _query_ sql2.sql
@@ -1,4 +1,4 @@
-	What is the total no. of births for all the countries in the dataset?
+#What is the total no. of births for all the countries in the dataset?
   
   select County_of_Residence as country, sum(Births) as no_of_births from `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality`
   group by County_of_Residence
