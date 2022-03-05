062cf60fed56e97184bd5b87f6f426a597e5271e
devisettyrohita <100766371+devisettyrohita@users.noreply.github.com>
2022-03-05 23:11:48 +0530

    Update Big _query_ sql10.sql

--- Big _query_ sql10.sql
+++ Big _query_ sql10.sql
@@ -1,1 +1,5 @@
-#How does father's age race react to the abnormalities in children?
+#what is the relationship between the county of residence and abnormal pregnancy?
+SELECT  aa.County_of_Residence, aa.Abnormal_Conditions_Checked_YN
+FROM  `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_abnormal_conditions`aa
+INNER JOIN  `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_father_race`bb
+ON aa.County_of_Residence= bb.County_of_Residence
